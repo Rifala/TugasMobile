5eec1b7fd411921762c58c44d35d3ad97eb2f55b
Rifala <92709287+Rifala@users.noreply.github.com>
2021-12-27 08:54:16 +0700

    Add files via upload

---
+++ TugasMobile/app/src/main/java/com/example/tugasmobile/Week_5.kt
@@ -0,0 +1,11 @@
+package com.example.tugasmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Week_5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week5)
+    }
+}
---
+++ TugasMobile/app/src/main/java/com/example/tugasmobile/Week_6.kt
@@ -0,0 +1,14 @@
+package com.example.tugasmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Week_6 : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week6)
+
+    }
+}
